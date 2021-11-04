6ff27c08884f6f6df2982aa33a08eb91c6b9a83d
SychWalley <synchwallet21@gmail.com>
2021-11-04 20:52:26 +0100

    Fix

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
   return res.status(201).json({
